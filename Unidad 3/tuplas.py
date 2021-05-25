7a9815793d5aa4a68d88470ed50916f99035e91d
federicomontesgarcia <baudolino78@hotmail.com>
2021-05-25 11:08:58 -0500

    listas compuestas y tuplas
    actualizacion mayo 25

---
+++ Unidad 3/tuplas.py
@@ -0,0 +1,29 @@
+t = ('esto es una cadena', 'b', 'c', 'd', 'e')
+print(t)
+print(t[0])
+
+print("")
+
+t = tuple('lupines')
+print(t)
+print(t[1:3])
+
+print("")
+
+m = ['have', 'fun']
+x, y = m
+print(x)
+print(y)
+
+print("")
+
+d = {'c':10, 'b':1, 'a':22}
+t = list(d.items())
+print(t)
+t.sort()
+print(t)
+
+print("")
+
+
+
